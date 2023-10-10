ecc632d9234bde1b8eba9a776b7ccea9d598c006
Alan-Coiffard <alan.coiffar@gmail.com>
2023-10-10 11:44:41 +0200

    add table for results

---
+++ app/modules/table.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="./styles/table.css">
+
+<div class="table-responsive custom-table-responsive">
+    <table class="table custom-table">
+        <thead>
+            <tr>
+                <th scope="col">Order</th>
+                <th scope="col">Occupation</th>
+                <th scope="col">Contact</th>
+                <th scope="col">Education</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr scope="row">
+                <td>1392</td>
+                <td>Web Designer</td>
+                <td>+63 983 0962 971</td>
+                <td>NY University</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
