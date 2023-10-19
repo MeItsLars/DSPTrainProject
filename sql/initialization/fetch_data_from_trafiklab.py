485cf1e8ba3ddf5ae2226dffc8ebe87529f3b72f
Lars Jeurissen <38491967+MeItsLars@users.noreply.github.com>
2023-10-19 16:48:43 +0200

    Update fetch_data_from_trafiklab.py

--- sql/initialization/fetch_data_from_trafiklab.py
+++ sql/initialization/fetch_data_from_trafiklab.py
@@ -2,12 +2,12 @@ import os
 import shutil
 import zipfile
 
-data_url = 'https://api.resrobot.se/v2.1/gtfs/sweden.zip?accessId=1cea7f8f-1584-4cd2-bff7-8f5cc07d6361'
+data_url = 'https://opendata.samtrafiken.se/gtfs-sweden/sweden.zip?key=af66205e225149c3b7415cc43665b3a0'
 
-# Delete old data/gtfs-sverige-2 directory
-if os.path.exists('./data/gtfs-sverige-2'):
+# Delete old data/gtfs-sverige-3 directory
+if os.path.exists('./data/gtfs-sverige-3'):
     print('Deleting old data')
-    shutil.rmtree('./data/gtfs-sverige-2')
+    shutil.rmtree('./data/gtfs-sverige-3')
 
 # Create data folder
 if not os.path.exists('data'):
@@ -15,15 +15,16 @@ if not os.path.exists('data'):
 
 # Download the data
 print('Downloading data from', data_url)
-os.system('curl -o ./data/gtfs-sverige-2.zip ' + data_url)
+
+os.system('curl -H "Accept-Encoding: gzip" -o ./data/gtfs-sverige-3.zip ' + data_url)
 
 # Unzip the data
 print('Unzipping data')
-with zipfile.ZipFile('./data/gtfs-sverige-2.zip', 'r') as zip_ref:
-    zip_ref.extractall('./data/gtfs-sverige-2')
+with zipfile.ZipFile('./data/gtfs-sverige-3.zip', 'r') as zip_ref:
+    zip_ref.extractall('./data/gtfs-sverige-3')
 
 # Remove the zip file
 print('Removing zip file')
-os.remove('./data/gtfs-sverige-2.zip')
+os.remove('./data/gtfs-sverige-3.zip')
 
 print('Done')
