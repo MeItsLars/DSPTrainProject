3d09230618457a02796f146c9aa2e686e123be9f
Alan-Coiffard <alan.coiffar@gmail.com>
2023-10-20 10:49:30 +0200

    modify query and form verification

--- app/script/minInputDatatlist.js
+++ app/script/minInputDatatlist.js
@@ -1,21 +1,21 @@
-var sizeInput = 3;
+// var sizeInput = 3;
 
-var inputFrom       = document.querySelector("#from"); // Selects the input.
-var inputTo         = document.querySelector("#to"); // Selects the input.
-var datalistFrom    = document.querySelector(".from"); // Selects the datalist.
-var datalistTo      = document.querySelector(".to"); // Selects the datalist.
+// var inputFrom       = document.querySelector("#from"); // Selects the input.
+// var inputTo         = document.querySelector("#to"); // Selects the input.
+// var datalistFrom    = document.querySelector(".from"); // Selects the datalist.
+// var datalistTo      = document.querySelector(".to"); // Selects the datalist.
 
-inputFrom.addEventListener("keyup", (e) => {
-    if (e.target.value.length >= sizeInput) {
-        datalistFrom.setAttribute("id", "citynameFrom");
-    } else {
-        datalistFrom.setAttribute("id", "");
-    }
-});
-inputTo.addEventListener("keyup", (e) => {
-    if (e.target.value.length >= sizeInput) {
-        datalistTo.setAttribute("id", "citynameTo");
-    } else {
-        datalistTo.setAttribute("id", "");
-    }
-});
+// inputFrom.addEventListener("keyup", (e) => {
+//     if (e.target.value.length >= sizeInput) {
+//         datalistFrom.setAttribute("id", "citynameFrom");
+//     } else {
+//         datalistFrom.setAttribute("id", "");
+//     }
+// });
+// inputTo.addEventListener("keyup", (e) => {
+//     if (e.target.value.length >= sizeInput) {
+//         datalistTo.setAttribute("id", "citynameTo");
+//     } else {
+//         datalistTo.setAttribute("id", "");
+//     }
+// });
